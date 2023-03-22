0921f1723833b049cf65ee8d0c36ef954867bd25
Dongxiao <kuit@msn.com>
2023-03-22 18:00:23 +0800

    Update license content

--- launcher/launcher.cc
+++ launcher/launcher.cc
@@ -1,5 +1,5 @@
 /*
- * Copyright (c) 2022-2023 Elastos Foundation
+ *  Copyright (c) 2022 - 2023 trinity-tech.io
  *
  * Permission is hereby granted, free of charge, to any person obtaining a copy
  * of this software and associated documentation files (the "Software"), to deal
